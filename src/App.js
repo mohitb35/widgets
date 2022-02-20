bbdb74065e4639356a66d953560755ef61e0960c
Mohit Bajaj <mohit_b35@hotmail.com>
2022-02-20 17:28:34 +0530

    1. Scaffolding a. render Accordion compontent in app b. Pass items array to Accordion and render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Accordion from './components/Accordion';
+
+const items = [
+	{
+		title: 'What is React?',
+		content: 'React is a front end JS framework'
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favourite among front end engineers'
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components'
+	},
+]
+
+const App = () => {
+	return (
+		<div>
+			<Accordion items={items}/>
+		</div>
+	)
+};
 
 export default App;
