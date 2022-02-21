c106c05caed6298b9d51d0577b9890df7b577acb
Mohit Bajaj <mohit_b35@hotmail.com>
2022-02-21 16:33:11 +0530

    4.1 - modify search widget to remove dep. warning - use 2 useEffects depending on searchTerm, and on debouncedTerm resp. - 1st useEffect watches searchTerm and sets debouncedTerm with a timeout - 2nd useEffect triggers API call when debouncedTerm changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,34 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 const Search = () => {
 	const [ searchTerm, setSearchTerm ] = useState('');
+	const [ debouncedTerm, setDebouncedTerm ] = useState('');
 	const [ results, setResults ] = useState([]);
 
 	useEffect( () => {
-		async function fetchData() {
-			const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&utf8=1&srsearch=${encodeURIComponent(searchTerm)}`);
-			const data =  await response.json();
-			setResults(data.query.search);
-		  }
+		const timeoutId = setTimeout(() => {
+			setDebouncedTerm(searchTerm)
+		}, 500);
 
-		if (searchTerm && !results.length) {
-			fetchData(); 
-		} else {
-			// Note: you can also use state to store the previous timeout id, but timeout id will need to be shared as a dependency witin useEffect, and you will need to add checks to prevent the code from running in an infinite loop.
-			const timeoutId = setTimeout(() => {
-				if (searchTerm) {
-					fetchData()
-				}
-			}, 500);
-
-			// Cleanup function. Called before next time useEffect is run.
-			return(() => {
-				clearTimeout(timeoutId)
-			});
-		}
+		// Cleanup function. Called before next time useEffect is run.
+		return(() => {
+			clearTimeout(timeoutId)
+		});
 	}, [searchTerm])
 	// The 2nd parameter to useEffect configures when the callback should run. It can be empty(run on every render), [] (run only on 1st render), [data1, data2] (run on 1st render, and every rerender if the specified data changes)
 
-	// As results.length is used inside the useEffect callback, if not provided within the dependency array (2nd parameter), you will see an es-lint warning. If you provide results as a dependency, it will cause an infinite loop (as results is set from within the useEffect callback, triggering a rerender and useEffect being called again) unless you provide some way of escaping the loop.
+	// As results.length was used inside the useEffect callback previously and not provided within the dependency array (2nd parameter), we saw an es-lint warning. If you provide results as a dependency, it will cause an infinite loop (as results is set from within the useEffect callback, triggering a rerender and useEffect being called again) unless you provide some way of escaping the loop.
+
+	// We use a second useEffect function to call the API, whenever debouncedTerm changes. 
+
+	useEffect( () => {
+		async function fetchData() {
+			const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&utf8=1&srsearch=${encodeURIComponent(debouncedTerm)}`);
+			const data =  await response.json();
+			setResults(data.query.search);
+		  }
+		if (debouncedTerm) {
+			fetchData();
+		}
+	},[debouncedTerm])
 
 	const renderedResults = results.map( result => {
 		return (
