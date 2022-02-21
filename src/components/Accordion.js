fa0f32efa43dff994346827897eb38dbc4a0304a
Mohit Bajaj <mohit_b35@hotmail.com>
2022-02-21 10:45:19 +0530

    3. add state to Accordion - with useState hook

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+	// Declare a state variable
+	const [activeIndex, setActiveIndex] = useState(null);
+
 	const onTitleClick = (index) => {
-		console.log("Item clicked:", index);
+		setActiveIndex(index);
 	};
 
 	const renderedItems = items.map((item, index) => {
+		const active = index === activeIndex ? 'active' : '';
+
 		return (
 			<React.Fragment key={item.title}> 
 				<div 
-					className="title active"
+					className={`title ${active}`}
 					onClick={() => onTitleClick(index)}
 				>
 					<i className="dropdown icon"></i>
-					item.title
+					{item.title}
 				</div>
-				<div className="content active">
+				<div className={`content ${active}`}>
 					<p>{item.content}</p>
 				</div>
 			</React.Fragment>
